a3b1b678d7fc1a94e24a09857a47cb36adee7374
Hichame Benaddi <hbenaddi@c1r6p1.42lehavre.fr>
2024-06-01 22:12:24 +0200

    ddd

--- src/affichage.c
+++ src/affichage.c
@@ -6,7 +6,7 @@
 /*   By: hbenaddi <hbenaddi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 15:47:18 by hbenaddi          #+#    #+#             */
-/*   Updated: 2024/05/27 23:44:55 by hbenaddi         ###   ########.fr       */
+/*   Updated: 2024/06/01 22:01:07 by hbenaddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,12 +105,11 @@ void	display_moove_count(t_game *game, int start)
 {
 	char	*move_str;
 
-	move_str = ft_itoa(game->moove_count);
+	move_str = ft_itoa(game->step);
 	if (!start)
 	{
 		mlx_delete_image(game->mlx, game->tab_png->steps_count);
-		game->tab_png->steps_count = mlx_put_string(game->mlx, \
-		move_str, 30, 10);
+		game->tab_png->steps_count = mlx_put_string(game->mlx, move_str, 30, 10);
 		free(move_str);
 	}
 }
